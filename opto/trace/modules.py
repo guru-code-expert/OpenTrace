f7d4bb17c62be7b2377e532c7569c3ad628c8dce
windweller <leo.niecn@gmail.com>
2025-06-10 14:36:34 -0700

    initial commit

--- opto/trace/modules.py
+++ opto/trace/modules.py
@@ -1,6 +1,8 @@
 import os
 import pickle
 import copy
+import inspect
+import textwrap
 from opto.trace.containers import ParameterContainer
 from opto.trace.nodes import ParameterNode
 
@@ -11,7 +13,22 @@ def model(cls):
     """
 
     class ModelWrapper(cls, Module):
-        pass
+        def model_dump(self, filename):
+            methods = [
+                method for name, method in cls.__dict__.items()
+                if inspect.isfunction(method)
+            ]
+
+            with open(filename, "w") as f:
+                f.write(f"class {cls.__name__}:\n")
+
+                for i, method in enumerate(methods):
+                    source = inspect.getsource(method)
+                    source = textwrap.dedent(source)
+                    indented = textwrap.indent(source, "    ")
+                    f.write(indented)
+                    if i < len(methods) - 1:
+                        f.write("\n")  # only one newline between methods
 
     return ModelWrapper
 
@@ -25,8 +42,8 @@ class Module(ParameterContainer):
     def __call__(self, *args, **kwargs):
         return self.forward(*args, **kwargs)
 
-    def save(self, file_name):
-        """Save the parameters of the model to a file."""
+    def save(self, file_name: str):
+        """Save the parameters of the model to a pickle file."""
         # detect if the directory exists
         directory = os.path.dirname(file_name)
         if directory != "":
@@ -35,7 +52,7 @@ class Module(ParameterContainer):
             pickle.dump(copy.deepcopy(self.parameters_dict()), f)
 
     def load(self, file_name):
-        """Load the parameters of the model from a file."""
+        """Load the parameters of the model from a pickle file."""
         with open(file_name, "rb") as f:
             loaded_data = pickle.load(f)
         self._set(loaded_data)
