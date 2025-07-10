d3bf800f15ac365d23a1849e4b66eec9f130eb00
chinganc <chinganc0@gmail.com>
2025-07-10 20:15:46 +0000

    Add randomize flag and n_epochs attribute to Dataloader.

--- opto/trainer/loader.py
+++ opto/trainer/loader.py
@@ -5,12 +5,15 @@ import numpy as np
 
 class DataLoader:
 
-    def __init__(self, dataset, batch_size=1, replacement=False, shuffle=True):
+    def __init__(self, dataset, batch_size=1, randomize=True, replacement=False, shuffle=True):
         """ Initialize the data loader
 
         Args:
             dataset: the dataset to load (a dict of inputs and infos)
             batch_size: the number of samples to load in each batch
+            randomize: whether to randomize the dataset ordering before loading;
+                       if False, the dataset will be loaded in the order it is
+                       provided (replacement and shuffle be ignored)
             replacement: whether to sample with replacement
             shuffle: whether to shuffle the dataset after each epoch
         """
@@ -20,9 +23,11 @@ class DataLoader:
 
         self.dataset = dataset
         self.batch_size = batch_size
+        self.randomize = randomize
         self.replacement = replacement
         self.shuffle = shuffle
         self._indices = self._update_indices()
+        self.n_epochs = 0
         self._i = 0
 
     def __iter__(self):
@@ -33,7 +38,9 @@ class DataLoader:
         if self._i >= len(self._indices):
             if self.shuffle:
                 self._indices = self._update_indices()
+            # Reset the index for the next epoch
             self._i = 0
+            self.n_epochs += 1
             raise StopIteration
         indices = self._indices[self._i: min(self._i + self.batch_size, len(self._indices))]
         xs = [self.dataset['inputs'][ind] for ind in indices]
@@ -43,7 +50,10 @@ class DataLoader:
 
     def _update_indices(self):
         N = len(self.dataset['inputs'])
-        return np.random.choice(N, size=N, replace=self.replacement)
+        if self.randomize:
+            return np.random.choice(N, size=N, replace=self.replacement)
+        else:
+            return np.arange(N)
 
     def sample(self):
         """ Sample a batch of data from the dataset """
