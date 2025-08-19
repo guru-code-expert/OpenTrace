e32e379ec6ccd1aa0fe8b3e34eb3db9fb61d89dc
chinganc <chinganc0@gmail.com>
2025-08-19 23:54:37 +0000

    Fix missing oprov2 problem

--- opto/optimizers/__init__.py
+++ opto/optimizers/__init__.py
@@ -1,9 +1,10 @@
 from opto.optimizers.optoprime import OptoPrime as OptoPrimeV1
 from opto.optimizers.optoprimemulti import OptoPrimeMulti
 from opto.optimizers.opro import OPRO
+from opto.optimizers.opro_v2 import OPROv2
 from opto.optimizers.textgrad import TextGrad
 from opto.optimizers.optoprime_v2 import OptoPrimeV2
 
 OptoPrime = OptoPrimeV1
 
-__all__ = ["OPRO", "OptoPrime", "OptoPrimeMulti", "TextGrad", "OptoPrimeV2", "OptoPrimeV1"]
+__all__ = ["OPRO", "OptoPrime", "OptoPrimeMulti", "TextGrad", "OptoPrimeV2", "OptoPrimeV1", "OPROv2"]
