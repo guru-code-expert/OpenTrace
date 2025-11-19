0126bfbbf8342e84af743e417fd98fbce64d18de
windweller <leo.niecn@gmail.com>
2025-11-19 17:54:43 -0500

    adding a modal environment for dev (with support on filesync/vscode/TCP port and jupyter notebook).

---
+++ dev_deployment/README.md
@@ -0,0 +1,5 @@
+# Starting a Dev Environment on Modal
+
+```
+modal run trace_dev_modal_image.py
+```
---
+++ dev_deployment/trace_dev_modal_image.py
@@ -0,0 +1,65 @@
+"""
+This sets up a Modal container that runs a Jupyter Lab.
+It will:
+1. Expose a tunnel for ssh and for jupyter (with public URL link)
+2. Attach a persistent volume (for data storage).
+"""
+
+import os
+import secrets
+import subprocess
+from pathlib import Path
+
+import modal
+
+# Run this command: `modal volume create trace-dev`
+volume = modal.Volume.from_name("trace-dev")
+
+ssh_key_path = Path.home() / ".ssh" / "id_rsa_modal.pub"
+
+image = (
+    modal.Image.debian_slim()
+    .pip_install("jupyter")
+    .apt_install("openssh-server")
+    .run_commands("mkdir /run/sshd")
+    .add_local_file(str(ssh_key_path), "/root/.ssh/authorized_keys", copy=True)
+)
+app = modal.App("Trace-dev-jupyter", image=image)
+
+@app.function(volumes={"/vol/trace-dev-home/": volume}, timeout=3600)
+def run_jupyter():
+
+    # a nested setup to start SSH connection and Jupyter
+
+    subprocess.Popen(["/usr/sbin/sshd", "-D", "-e"])
+
+    with modal.forward(port=22, unencrypted=True) as ssh_tunnel:
+        hostname, port = ssh_tunnel.tcp_socket
+        connection_cmd = f'ssh -p {port} root@{hostname}'
+        print("Run `ssh-add ~/.ssh/id_rsa_modal` to add SSH key credential")
+        print(f"ssh into container using: {connection_cmd}")
+
+        token = secrets.token_urlsafe(13)
+
+        with modal.forward(8888) as tunnel:
+            url = tunnel.url + "/?token=" + token
+            print(f"Starting Jupyter at {url}")
+            subprocess.run(
+                [
+                    "jupyter",
+                    "notebook",
+                    "--no-browser",
+                    "--allow-root",
+                    "--ip=0.0.0.0",
+                    "--port=8888",
+                    "--LabApp.allow_origin='*'",
+                    "--LabApp.allow_remote_access=1",
+                ],
+                env={**os.environ, "JUPYTER_TOKEN": token, "SHELL": "/bin/bash"},
+                stderr=subprocess.DEVNULL,
+        )
+
+@app.local_entrypoint()
+def main():
+    # Server()
+    run_jupyter.remote()
