43b150a8703bd4dedaf94b05fe84425eb88a1fbe
windweller <leo.niecn@gmail.com>
2025-11-19 18:22:01 -0500

    improve some usability

--- dev_deployment/trace_dev_modal_image.py
+++ dev_deployment/trace_dev_modal_image.py
@@ -2,7 +2,11 @@
 This sets up a Modal container that runs a Jupyter Lab.
 It will:
 1. Expose a tunnel for ssh and for jupyter (with public URL link)
-2. Attach a persistent volume (for data storage).
+2. Add Trace repo code into the container and include it in the PYTHONPATH
+3. Attach a persistent volume (for data storage).
+
+The code lives in the ephemeral space, but any results should be saved in the persistent volume,
+mounted under `/vol/trace-dev-home/`.
 """
 
 import os
@@ -12,21 +16,29 @@ from pathlib import Path
 
 import modal
 
+MINUTES = 60  # seconds
+HOURS = 60 * MINUTES
+
 # Run this command: `modal volume create trace-dev`
 volume = modal.Volume.from_name("trace-dev")
 
 ssh_key_path = Path.home() / ".ssh" / "id_rsa_modal.pub"
+project_root = Path(__file__).parent.parent
 
 image = (
     modal.Image.debian_slim()
     .pip_install("jupyter")
     .apt_install("openssh-server")
+    .env({
+        "PYTHONPATH": "/root/Trace"  # add KernelBench to python path
+    })
     .run_commands("mkdir /run/sshd")
     .add_local_file(str(ssh_key_path), "/root/.ssh/authorized_keys", copy=True)
+    .add_local_dir(str(project_root), "/root/Trace", copy=True)
 )
 app = modal.App("Trace-dev-jupyter", image=image)
 
-@app.function(volumes={"/vol/trace-dev-home/": volume}, timeout=3600)
+@app.function(volumes={"/vol/trace-dev-home/": volume}, timeout=24 * HOURS)
 def run_jupyter():
 
     # a nested setup to start SSH connection and Jupyter
@@ -61,5 +73,5 @@ def run_jupyter():
 
 @app.local_entrypoint()
 def main():
-    # Server()
+    subprocess.run(["ssh-add", "~/.ssh/id_rsa_modal"])
     run_jupyter.remote()
